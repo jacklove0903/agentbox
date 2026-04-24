b13b315d1966ddb14c106ab2236570cadfa539a1
jacklove0903 <1725720075@qq.com>
2026-04-24 11:34:41 +0800

    chore: 替换 favicon 为自定义紫蓝渐变 A 图标（SVG）

--- agentbox-frontend/src/app/layout.tsx
+++ agentbox-frontend/src/app/layout.tsx
@@ -5,9 +5,6 @@ import { ClientBody } from "./ClientBody";
 export const metadata: Metadata = {
   title: "agentbox",
   description: "agentbox - 多模型并行对话平台",
-  icons: {
-    icon: "https://ext.same-assets.com/2425995810/1211505447.png",
-  },
 };
 
 export default function RootLayout({
